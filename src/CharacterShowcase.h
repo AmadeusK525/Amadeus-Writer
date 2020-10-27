9a071e9f780945c7e3495ebe84740b01373aa1d0
AmadeusK525 <bronstein.franceschetti@gmail.com>
2020-10-27 01:08:07 -0300

    Made fields unselectable

--- src/CharacterShowcase.h
+++ src/CharacterShowcase.h
@@ -12,24 +12,26 @@ using std::vector;
 
 class CharacterShowcase : public wxScrolledWindow {
 private:
-    ImagePanel* image = nullptr;
-    wxTextCtrl* label1 = nullptr;
-    wxTextCtrl* age = nullptr;
-    wxTextCtrl* sex = nullptr;
-    wxTextCtrl* nat = nullptr;
-    wxTextCtrl* height = nullptr;
-    wxTextCtrl* nick = nullptr;
-    wxTextCtrl* role = nullptr;
-    wxTextCtrl* appearance = nullptr;
-    wxTextCtrl* personality = nullptr;
-    wxTextCtrl* backstory = nullptr;
+	ImagePanel* image = nullptr;
+	wxStaticText* label1 = nullptr;
 
-    wxBoxSizer* vertical = nullptr;
-    wxBoxSizer* apbSizer = nullptr;
+	wxStaticText* age = nullptr,
+		* sex = nullptr,
+		* nat = nullptr,
+		* height = nullptr,
+		* nick = nullptr,
+		* role = nullptr;
+
+	wxTextCtrl* appearance = nullptr,
+		* personality = nullptr,
+		* backstory = nullptr;
+
+	wxBoxSizer* vertical = nullptr;
+	wxBoxSizer* apbSizer = nullptr;
 
 public:
-    CharacterShowcase(wxWindow* parent);
+	CharacterShowcase(wxWindow* parent);
 
-    void setData(wxImage& image, vector<string>& charData);
+	void setData(wxImage& image, vector<string>& charData);
 };
 
