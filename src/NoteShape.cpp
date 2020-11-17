f0e6dd90eb9f9054fa2571c4c41b5b15b74bde91
AmadeusK525 <bronstein.franceschetti@gmail.com>
2020-11-17 09:22:37 -0300

    Added image shape and colors to the note shapes. Started messing around with connections.

--- src/NoteShape.cpp
+++ src/NoteShape.cpp
@@ -4,13 +4,14 @@
 XS_IMPLEMENT_CLONABLE_CLASS(NoteShape,
 	wxSFRoundRectShape);
 
-NoteShape::NoteShape(): wxSFRoundRectShape() {
+NoteShape::NoteShape() : wxSFRoundRectShape() {
 	// Set accepted connections for the new shape.
 	AcceptConnection("All");
 	AcceptSrcNeighbour("NoteShape");
 	AcceptTrgNeighbour("NoteShape");
+	AcceptChild("None");
 
-	SetRadius(8.0);
+	SetRadius(6.0);
 	SetFill(wxBrush(wxColour(80, 80, 80)));
 
 	AddStyle(sfsNO_FIT_TO_CHILDREN);
@@ -23,25 +24,27 @@ NoteShape::NoteShape(): wxSFRoundRectShape() {
 
 		content->SetVBorder(10.0);
 
-		//content->SetFill(wxBrush(wxColour(230, 230, 230)));
-		
 		// Set required shape style(s)
-		content->SetStyle(sfsALWAYS_INSIDE | sfsHOVERING | sfsPROCESS_DEL | sfsPROPAGATE_SELECTION);
-		
+		content->SetStyle(sfsALWAYS_INSIDE | sfsHOVERING | sfsPROCESS_DEL |
+			sfsPROPAGATE_SELECTION | sfsPROPAGATE_DRAGGING | sfsPROPAGATE_INTERACTIVE_CONNECTION);
+
 		// Components of composite shapes created at runtime in parent
 		// shape constructor cannot be re-created by the serializer so
 		// it is important to disable their automatic serialization ...
 		content->EnableSerialization(false);
-		
+
 		// ... but their properties can be serialized in the standard way:
 		XS_SERIALIZE_DYNAMIC_OBJECT_NO_CREATE(content, "content");
-		
+
 		// Assign the text shape to the parent shape.
 		AddChild(content);
 	}
+
+	Scale(2.3, 2.6);
+	content->SetText("New note");
 }
 
-NoteShape::NoteShape(static NoteShape& other) : wxSFRoundRectShape(other) {
+NoteShape::NoteShape(const NoteShape& other) : wxSFRoundRectShape(other) {
 	// Clone source child text object..
 	content = (AutoWrapTextShape*)other.content->Clone();
 
@@ -53,6 +56,70 @@ NoteShape::NoteShape(static NoteShape& other) : wxSFRoundRectShape(other) {
 		// not by the serializer (only its properties are deserialized.
 		XS_SERIALIZE_DYNAMIC_OBJECT_NO_CREATE(content, "content");
 	}
+}
+
+void NoteShape::changeColour(wxWindowID id) {
+	wxColour frameColour, textBgColour, textFgColour;
+	textFgColour = wxColour(20, 20, 20);
+
+	switch (id) {
+	case CorkboardCanvas::MENU_NoteDefault:
+		frameColour = wxColour(80, 80, 80);
+		textBgColour = wxColour(255, 255, 255);
+
+		break;
+
+	case CorkboardCanvas::MENU_NoteBlack:
+		frameColour = wxColour(20, 20, 20);
+		textBgColour = wxColour(80, 80, 80);
+		textFgColour = wxColour(230, 230, 230);
+
+		break;
+
+	case CorkboardCanvas::MENU_NoteWhite:
+		frameColour = wxColour(230, 230, 230);
+		textBgColour = wxColour(255, 255, 255);
+
+		break;
+
+	case CorkboardCanvas::MENU_NoteRed:
+		frameColour = wxColour(200, 0, 0);
+		textBgColour = wxColour(200, 100, 100);
+		textFgColour = wxColour(20, 20, 20);
+
+		break;
+
+	case CorkboardCanvas::MENU_NoteGreen:
+		frameColour = wxColour(0, 180, 0);
+		textBgColour = wxColour(130, 200, 130);
+		textFgColour = wxColour(10, 10, 10);
+
+		break;
+
+	case CorkboardCanvas::MENU_NoteBlue:
+		frameColour = wxColour(0, 0, 180);
+		textBgColour = wxColour(130, 130, 200);
+
+		break;
+
+	case CorkboardCanvas::MENU_NoteYellow:
+		frameColour = wxColour(245, 245, 0);
+		textBgColour = wxColour(255, 255, 130);
+
+		break;
+
+	case CorkboardCanvas::MENU_NotePink:
+		frameColour = wxColour(255, 20, 147);
+		textBgColour = wxColour(255, 182, 193);
+
+		break;
+	}
+
+	SetFill(frameColour);
+	content->SetFill(textBgColour);
+	content->SetTextColour(textFgColour);
+
+	curColour = id;
 }
 
 void NoteShape::OnBeginHandle(wxSFShapeHandle& handle) {
