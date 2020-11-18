efbfa3c8ec305871b89c2c548bb4f575fdd7c211
AmadeusK525 <bronstein.franceschetti@protonmail.com>
2020-11-18 19:53:59 -0300

    Changed how images are resized and some other minor stuff.

--- src/ImageShape.cpp
+++ src/ImageShape.cpp
@@ -1,4 +1,5 @@
 #include "ImageShape.h"
+#include "CorkboardCanvas.h"
 
 XS_IMPLEMENT_CLONABLE_CLASS(ImageShape, wxSFBitmapShape)
 
@@ -12,14 +13,60 @@ ImageShape::ImageShape(const ImageShape& other): wxSFBitmapShape(other) {
 
 ImageShape::~ImageShape() {}
 
-void ImageShape::Scale(double x, double y, bool children) {
-	if (m_fCanScale) {
-		m_nRectSize.x *= x;
-		m_nRectSize.y *= x;
+void ImageShape::create(const wxString& path, wxBitmapType type) {
+	CreateFromFile(path, type);
+	wxImage image(path);
+	
+	int width = image.GetWidth();
+	int height = image.GetHeight();
 
-		if (!m_fRescaleInProgress) RescaleImage(m_nRectSize);
+	ratio = (double)width / (double)height;
+}
 
-		// call default function implementation (needed for scaling of shape's children)
-		wxSFShapeBase::Scale(x, x, children);
+void ImageShape::OnBeginHandle(wxSFShapeHandle& handle){
+	wxSFBitmapShape::OnBeginHandle(handle);
+}
+
+void ImageShape::OnHandle(wxSFShapeHandle& handle) {
+	wxRect rect(GetBoundingBox());
+	wxPoint dxy(handle.GetDelta());
+
+	switch (handle.GetType()) {
+	case wxSFShapeHandle::hndRIGHTBOTTOM:
+	case wxSFShapeHandle::hndBOTTOM:
+		m_nRectSize.y += dxy.y;
+		m_nRectSize.x = m_nRectSize.y * ratio;
+		break;
+	case wxSFShapeHandle::hndLEFTBOTTOM:
+		m_nRectSize.x -= dxy.x;
+		m_nRectSize.y = m_nRectSize.x / ratio;
+		m_nRelativePosition.x += dxy.x;
+		break;
+	case wxSFShapeHandle::hndRIGHT:
+		m_nRectSize.x += dxy.x;
+		m_nRectSize.y = m_nRectSize.x / ratio;
+		break;
+	case wxSFShapeHandle::hndLEFT:
+		m_nRectSize.x -= dxy.x;
+		m_nRectSize.y = m_nRectSize.x / ratio;
+		m_nRelativePosition.x += dxy.x;
+		break;
+	case wxSFShapeHandle::hndLEFTTOP:
+	{
+		m_nRectSize.x -= dxy.x;
+		double prevY = m_nRectSize.y;
+		m_nRectSize.y = m_nRectSize.x / ratio;
+		m_nRelativePosition.x += dxy.x;
+		m_nRelativePosition.y += prevY - m_nRectSize.y;
+		break;
 	}
+	case wxSFShapeHandle::hndRIGHTTOP:
+	case wxSFShapeHandle::hndTOP:
+		m_nRectSize.y -= dxy.y;
+		m_nRectSize.x = m_nRectSize.y * ratio;
+		m_nRelativePosition.y += dxy.y;
+		break;
+	}
+
+	//Scale(ratio, ratio);
 }
