90d79a2907a17afc4af920c3d5d58454e783d4b6
AmadeusK525 <bronstein.franceschetti@protonmail.com>
2021-05-20 17:04:20 -0300

    Removed unused files, which have been useless for months.

--- src/Note.cpp
+++
@@ -1,6 +0,0 @@
-#include "Note.h"
-
-Note::Note(wxString content, wxString name) {
-    this->content = content;
-    this->name = name;
-}
--- src/Note.h
+++
@@ -1,7 +0,0 @@
-#ifndef NOTE_H_
-#define NOTE_H_
-#pragma once
-
-#include <wx\wx.h>
-
-#endif
